2ac7dd619836356a06c22899630e41a7f407a9f6
Uday Yadav <dev117uday@gmail.com>
2021-07-19 00:09:05 +0530

    added dbcli

---
+++ dbcli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dapper;
+using Newtonsoft.Json;
+using Npgsql;
+
+namespace dbcli
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var cs = "User ID=postgres;Password=password;Host=localhost;Port=5432;Database=firsttemp;Pooling=true;";
+			var db = new NpgsqlConnection(cs);
+			db.Open();
+		}
+	}
+}
